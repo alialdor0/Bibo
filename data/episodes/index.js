3d50d3fc57c6eb261c91c5ce44a123d1bf4e0e24
alialdor0 <hly193692@gmail.com>
2026-07-10 17:22:45 +0300

    Update index.js

--- data/episodes/index.js
+++ data/episodes/index.js
@@ -3,11 +3,19 @@
 // season مستقل؛ لو المسار مالوش ملف هنا، Story.js بيرجع للنظام القديم
 // (STORY_LINES) أو شاشة "قريبًا".
 
-import spySeason1 from './spy_season1.json';
+import spySeason1     from './spy_season1.json';
+import medicalSeason1 from './medical_season1.json';
+import loveSeason1    from './love_season1.json';
+import crimeSeason1   from './crime_season1.json';
+import familySeason1  from './family_season1.json';
 
 // trackId -> season data (episodes[], project, level, ...)
 export const SEASONS = {
-  spy: spySeason1,
+  spy:     spySeason1,
+  medical: medicalSeason1,
+  love:    loveSeason1,
+  crime:   crimeSeason1,
+  family:  familySeason1,
 };
 
 export function hasEpisodes(trackId) {
